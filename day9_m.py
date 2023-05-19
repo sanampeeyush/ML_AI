acfb300eeb207153b8ed5e1489ee17230ed27f6d
sanampeeyush <peeyush.sanam@gmail.com>
2023-05-19 14:00:14 +0530

    face reco

--- day9_m.py
+++
@@ -1,4 +0,0 @@
-import cv2
-img = cv2.imread('robot-upflairs.png')
-cv2.imshow('',img)
-cv2.waitKey(0)
--- morning/day18_sp.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-import speech_recognition as sr
-import nltk
-
-spEng = pyttsx3.init()
-spEng.setProperty('rate',120)
-
-spEng.say('Hi')
-spEng.runAndWait()
